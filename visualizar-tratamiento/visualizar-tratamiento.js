ead2380bcf30572c487503dd0f3a0bed8b9f615c
JossyFQ <147462438+JossyFQ@users.noreply.github.com>
2024-06-13 12:31:15 -0500

    Add files via upload

---
+++ visualizar-tratamiento/visualizar-tratamiento.js
@@ -0,0 +1,9 @@
+function mensaje() {
+
+}
+function xml() {
+    window.location.href = 'visualizar-tratamiento.xml';
+}
+function json() {
+    window.location.href = 'visualizar-tratamiento.json';
+}
